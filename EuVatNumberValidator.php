44731815bdf8dd4317747aee0f6918cee887ca72
Maris Graudins <maris.graudins91@gmail.com>
2016-04-03 12:00:39 +0300

    Fixed EU Vat Number validator patterns.

--- EuVatNumberValidator.php
+++ EuVatNumberValidator.php
@@ -2,7 +2,7 @@
 namespace mgcode\validators;
 
 use Yii;
-use yii\validators\RegularExpressionValidator;
+use yii\validators\Validator;
 
 /**
  * Validates european vat number
@@ -16,12 +16,68 @@ use yii\validators\RegularExpressionValidator;
  * }
  * ```
  */
-class EuVatNumberValidator extends RegularExpressionValidator
+class EuVatNumberValidator extends Validator
 {
-    /** @inheritdoc */
+    /**
+     * @inheritdoc
+     */
     public function init()
     {
-        $this->pattern = '#^((AT)?U[0-9]{8}|(BE)?0[0-9]{9}|(BG)?[0-9]{9,10}|(CY)?[0-9]{8}L|(CZ)?[0-9]{8,10}|(DE)?[0-9]{9}|(DK)?[0-9]{8}|(EE)?[0-9]{9}|(EL|GR)?[0-9]{9}|(ES)?[0-9A-Z][0-9]{7}[0-9A-Z]|(FI)?[0-9]{8}|(FR)?[0-9A-Z]{2}[0-9]{9}|(GB)?([0-9]{9}([0-9]{3})?|[A-Z]{2}[0-9]{3})|(HU)?[0-9]{8}|(IE)?[0-9]S[0-9]{5}L|(IT)?[0-9]{11}|(LT)?([0-9]{9}|[0-9]{12})|(LU)?[0-9]{8}|(LV)?[0-9]{11}|(MT)?[0-9]{8}|(NL)?[0-9]{9}B[0-9]{2}|(PL)?[0-9]{10}|(PT)?[0-9]{9}|(RO)?[0-9]{2,10}|(SE)?[0-9]{12}|(SI)?[0-9]{8}|(SK)?[0-9]{10})$#';
         parent::init();
+        if ($this->message === null) {
+            $this->message = Yii::t('yii', '{attribute} is invalid.');
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function validateValue($value)
+    {
+        $valid = false;
+        if(!is_array($value)) {
+            foreach($this->patterns() as $pattern) {
+                if(preg_match($pattern, $value)) {
+                    $valid = true;
+                    break;
+                }
+            }
+        }
+
+        return $valid ? null : [$this->message, []];
+    }
+
+    protected function patterns()
+    {
+        return [
+            '#^ATU[A-Z0-9]{8,8}$#',
+            '#^BE[0-9]{10,10}$#',
+            '#^BG[0-9]{9,9}$|^BG[0-9]{10,10}$#',
+            '#^CY[0-9]{8,8}[A-Z]{1,1}$#',
+            '#^CZ[0-9]{8,10}$#',
+            '#^DE[0-9]{9,9}$#',
+            '#^DK[0-9]{8,8}$#',
+            '#^EE[0-9]{9,9}$#',
+            '#^ES[A-Z0-9]{1,1}[0-9]{7,7}[A-Z0-9]{1,1}$#',
+            '#^FI[0-9]{8,8}$#',
+            '#^FR[A-Z0-9]{2,2}[0-9]{9,9}$#',
+            '#^GB[0-9]{9,9}$|^GB[0-9]{12,12}$|^GBGD[0-9]{3,3}$|^GBHA[0-9]{3,3}$#',
+            '#^HU[0-9]{8,8}$#',
+            '#^IE[0-9]{1,1}[A-Z0-9]{1,1}[0-9]{5,5}[A-Z]{1,1}$|^IE[0-9]{7,7}[A-W]{1,1}[A-I]{1,1}$#',
+            '#^IT[0-9]{11,11}$#',
+            '#^LT[0-9]{9,9}$|^LT[0-9]{12,12}$#',
+            '#^LU[0-9]{8,8}$#',
+            '#^LV[0-9]{11,11}$#',
+            '#^MT[0-9]{8,8}$#',
+            '#^NL[A-Z0-9]{9,9}B[A-Z0-9]{2,2}$#',
+            '#^PL[0-9]{10,10}$#',
+            '#^PT[0-9]{9,9}$#',
+            '#^SE[0-9]{10,10}01$#',
+            '#^SI[0-9]{8,8}$#',
+            '#^SK[0-9]{10,10}$#',
+            '#^RO[1-9]{1,1}[0-9]{1,9}$#',
+            '#^EL[0-9]{9,9}$#',
+            '#^HR[0-9]{11,11}$#',
+        ];
     }
 }
